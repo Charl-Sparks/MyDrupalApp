0f78f8b93cf84b4f7eba83a10f6dbe2abce9fe6d
Charl Albre Sparks <charl_sparks@hotmail.com>
2013-10-17 12:30:22 +1300

    Drupal App

--- js/main.js
+++ js/main.js
@@ -1,30 +1,20 @@
 var app = {
 
     renderHomeView: function() {
-    var html =
-            "<div class='header'><h1>iAudit</h1></div>" +
-            "<div class='search-view'>" +
-            "<input class='search-key topcoat-search-input--large'/>" +
-            "<ul class='form-list'></ul>" +
-            "</div>"
-    $('body').html(html);
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-},
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
 
     findByName: function() {
-        console.log('findByName');
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.employee-list').html(self.employeeLiTpl(employees));
         });
     },
 
     initialize: function() {
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         var self = this;
         this.store = new WebSqlStore(function() {
             self.renderHomeView();
